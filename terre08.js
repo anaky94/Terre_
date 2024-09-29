c0d47d18b97937ed21158962315c5f507a1bed55
anaky94 <anaky.44@gmail.com>
2024-09-29 21:25:57 +0200

    leran

---
+++ terre08.js
@@ -0,0 +1,28 @@
+
+
+/*Créez un programme qui affiche le nombre de caractères d’une chaîne de caractères passée en argument.
+
+
+Exemples d’utilisation :
+$> python exo.py “Hello world!”
+12
+
+
+$> python exo.py
+erreur.
+
+
+$> python exo.py “Bonjour” “Aurevoir”
+erreur.
+
+$> python exo.py 10
+erreur.
+
+
+Fonctions interdites: 
+-La fonction length
+-La fonction size */
+for (let i = chaineArgumentsRevers.length - 1; i >= 0; i--) {
+
+    console.log ()
+}
