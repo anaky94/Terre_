c6a52e9875fc450c36f2a470c07dfcb617145249
anaky94 <anaky.44@gmail.com>
2025-01-10 10:35:12 +0100

    terre ok

--- terre15.js
+++ terre15.js
@@ -2,30 +2,24 @@
 Créez un programme qui transforme une heure affichée en format 12h en une heure affichée au format 24h.
 */
 const heureDonnee = process.argv[2];
-const heureRegex =   /^([1-9]|1[0-2]):[0-5][0-9]\s?(AM|PM)$/i;
+const heureRegex =   /^([1-9]|1[0-2]):[0-5][0-9]$/;
 
 function heureAmEnPm(){
-    
     if (!heureRegex.test(heureDonnee)){
-        console.log("erreur : veillez entrez une heure valide du format HH:MM AM/PM. ");
+        console.log("erreur : veillez entrez une heure valide du format HH:MM");
         return; 
     }
 }
 // Extraction des heures et minutes
 const [heure, minute] = heureDonnee.split(":");
-const suffixe = heureDonnee.trim().slice(-2).toUpperCase(); //AM ou PM
-
 let heure24 = parseInt(heure, 10);
-
-// Conversion au format  24 heures 
-if (suffixe === "AM" && heure24 === 12){
-    heure24 = 0; //Minuit(12AM) devient 00
-}else if (suffixe === "PM" && heure24 !== 12 ){
-    heure24 += 12; // l'après-midi ajoute 12 heures 
+if (heure ===12){
+    heure = 0; // minuit 12 heures en 12h devient 00 heures en 24h
+}else {
+    heure24 +=12; 
 }
-const sortieSuffice = (heure24>12) ? "PM" : "AM";
 
 // Affichage de l'heure au format de 12heurs
-console.log(` il est ${heure24.toString().padStart(2,"0")}:${minute.padStart(2,"0")}`);
+console.log(`il est ${heure24.toString().padStart(2,"0")}:${minute.padStart(2,"0")}`);
 
 heureAmEnPm();
